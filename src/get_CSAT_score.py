2dd8771487b6f6b47f6b9887d36375cf9b24b9b5
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-23 15:05:40 +0100

    Specify the date to view satisfaction ratings from customers today

--- src/get_CSAT_score.py
+++ src/get_CSAT_score.py
@@ -5,7 +5,10 @@ import json
 def get_CSAT_score_for_today(DOMAIN, API_KEY, PASSWORD) -> None:
 
     # CSAT% = (Number of positive responses / Total number of responses) x 100
-    r = requests.get(f"https://{DOMAIN}.freshdesk.com/api/v2/surveys/satisfaction_ratings", auth=(API_KEY, PASSWORD))
+    r = requests.get(
+        f"https://{DOMAIN}.freshdesk.com/api/v2/surveys/satisfaction_ratings?created_since=2022-11-23T00:00:01Z",
+        auth=(API_KEY, PASSWORD),
+    )
 
     if r.status_code == 200:
         print("Request processed successfully, the response is given below")
