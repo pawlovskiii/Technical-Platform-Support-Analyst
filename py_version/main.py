43e8efafa6131a1d6c394eba676f9d94832e9323
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-30 19:31:28 +0100

    Delete unnecessary indentation

--- py_version/main.py
+++ py_version/main.py
@@ -2,7 +2,6 @@ from src.get_resolved_tickets import get_resolved_tickets_for_today
 from src.get_CSAT_score import get_CSAT_score_for_today
 
 
-
 def main() -> None:
 
     API_KEY = "W28pTo26fhCgwzze6O9b"
@@ -11,8 +10,6 @@ def main() -> None:
 
     get_CSAT_score_for_today(DOMAIN, API_KEY, PASSWORD)
     get_resolved_tickets_for_today(DOMAIN, API_KEY, PASSWORD)
-
-
 
 
 if __name__ == "__main__":
