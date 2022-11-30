b7eb07d71207ab39a408e6055b70487f9507703f
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-11-30 17:40:56 +0100

    Create a function for getting tickets for today in JavaScript

---
+++ get_resolved_tickets.js
@@ -0,0 +1,21 @@
+const unirest = require('unirest');
+
+const API_KEY = 'W28pTo26fhCgwzze6O9b';
+const DOMAIN = 'zoovu';
+const URL = `https://${DOMAIN}.freshdesk.com/api/v2/search/tickets?query="status:4%20AND%20due_by:"2022-11-23""`;
+const Request = unirest.get(URL);
+
+function get_resolved_tickets_for_today(API_KEY, Request) {
+	Request.auth({
+		user: API_KEY,
+		pass: 'X',
+		sendImmediately: true,
+	}).end(function (response) {
+		console.log(response.body);
+		if (response.status != 200) {
+			console.log('X-Request-Id :' + response.headers['x-request-id']);
+		}
+	});
+}
+
+get_resolved_tickets_for_today(API_KEY, Request);
